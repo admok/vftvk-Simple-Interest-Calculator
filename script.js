96fe805f77a247c4794c670ed28ea0e6d589d56b
Adrian Mok <admok89@gmail.com>
2022-04-28 16:32:14 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if(principal > 0) {
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,\<br\>" 
+            + "at an interest rate of <mark>" + rate + "%</mark>.\<br\>" 
+            + "You will receive an amount of <mark>" + interest + "</mark>,\<br\>"
+            + "in the year <mark>" + year + "</mark>"
+    } else {
+        alert("Please enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
         
